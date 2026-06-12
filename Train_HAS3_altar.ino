7949ca82efdcebb558dfa187586f5505f765e3fd
DESKTOP-PLMAB5D\이효주 <julie08051@naver.com>
2026-06-12 12:02:14 +0900

    Firmware Update v12

--- Train_HAS3_altar.ino
+++ Train_HAS3_altar.ino
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2022
  */
 
-#define FIRMWARE_VER 11
+#define FIRMWARE_VER 12
 #include "Train_HAS3_altar.h"
 
 //************************************************ Core1 ********************************************************************
